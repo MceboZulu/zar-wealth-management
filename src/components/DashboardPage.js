80ee891c3d097027f06e910bdc5782a4b2f422d0
MceboZulu <69901429+MceboZulu@users.noreply.github.com>
2025-05-03 23:01:13 +0200

    Update DashboardPage.js

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -9,7 +9,7 @@ import Footer from './Layout/Footer';
 import { Oval } from 'react-loader-spinner';
 
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-const API_BASE_URL = 'http://192.168.68.104:2727';  
+const API_BASE_URL = 'https://4eec-41-1-71-226.ngrok-free.app';  
 
 function DashboardPage() {
   const [userData, setUserData] = useState(null);
