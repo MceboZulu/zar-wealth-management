3ec8bb15c8851f999a1e96f906c45f601e30a72f
MceboZulu <69901429+MceboZulu@users.noreply.github.com>
2026-05-05 13:11:14 +0200

    Update Footer.js

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Footer.css'; // You can create a CSS file for styling
+import './Footer.css';
 
 function Footer() {
   return (
